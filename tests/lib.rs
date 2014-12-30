434fc8b0ed02c8aaeb66948a58fcb744536fa484
Chucky Ellison <cme@freefour.com>
2014-12-30 17:51:15 -0500

    improving tests

--- tests/lib.rs
+++ tests/lib.rs
@@ -1,20 +1,56 @@
 extern crate time;
-
 extern crate stopwatch;
-use stopwatch::{Stopwatch};
 
+use stopwatch::{Stopwatch};
+use std::num::SignedInt;
 use std::io::timer;
 use std::time::Duration;
 
+static SLEEP_MS: i64 = 50;
+static TOLERANCE_PERCENTAGE: f64 = 0.2;
+
 #[test]
-fn it_works() {
+fn elapsed_ms() {
+	let sw = Stopwatch::start_new();
+	timer::sleep(Duration::milliseconds(SLEEP_MS));
+	assert_sw_near(sw, SLEEP_MS);
+}
+
+#[test]
+fn stop() {
 	let mut sw = Stopwatch::start_new();
-
-	timer::sleep(Duration::milliseconds(200));
-
+	timer::sleep(Duration::milliseconds(SLEEP_MS));
 	sw.stop();
+	assert_sw_near(sw, SLEEP_MS);
 
-	println!("{}", sw.elapsed_ms());
+	timer::sleep(Duration::milliseconds(SLEEP_MS));
+	assert_sw_near(sw, SLEEP_MS);
+}
+
+#[test]
+fn resume_once() {
+	let mut sw = Stopwatch::start_new();
+	timer::sleep(Duration::milliseconds(SLEEP_MS));
+	sw.stop();	
+	assert_sw_near(sw, SLEEP_MS);
+	sw.start();
+	timer::sleep(Duration::milliseconds(SLEEP_MS));
+	assert_sw_near(sw, 2 * SLEEP_MS);
+}
+
+#[test]
+fn resume_twice() {
+	let mut sw = Stopwatch::start_new();
+	timer::sleep(Duration::milliseconds(SLEEP_MS));
+	sw.stop();	
+	assert_sw_near(sw, SLEEP_MS);
+	sw.start();
+	timer::sleep(Duration::milliseconds(SLEEP_MS));
+	sw.stop();
+	assert_sw_near(sw, 2 * SLEEP_MS);
+	sw.start();
+	timer::sleep(Duration::milliseconds(SLEEP_MS));
+	assert_sw_near(sw, 3 * SLEEP_MS);
 }
 
 #[test]
@@ -30,8 +66,33 @@ fn is_running() {
 #[test]
 fn restart() {
 	let mut sw = Stopwatch::start_new();
-	timer::sleep(Duration::milliseconds(50));
+	timer::sleep(Duration::milliseconds(SLEEP_MS));
 	sw.restart();
-	timer::sleep(Duration::milliseconds(50));
-	println!("{}", sw.elapsed_ms());
+	timer::sleep(Duration::milliseconds(SLEEP_MS));
+	assert_sw_near(sw, SLEEP_MS);
+}
+
+#[test]
+fn reset() {
+	let mut sw = Stopwatch::start_new();
+	timer::sleep(Duration::milliseconds(SLEEP_MS));
+	sw.reset();
+	assert!(!sw.is_running());
+	assert_eq!(sw.elapsed_ms(), 0);
+}
+
+
+
+/////////////// helpers
+
+fn assert_near(x: i64, y: i64, tolerance: i64) {
+	let diff = (x - y).abs();
+	if diff > tolerance {
+		panic!("Expected {}, got {}", x, y);
+	}
+}
+
+fn assert_sw_near(mut sw: Stopwatch, elapsed: i64) {
+	let tolerance_value = (TOLERANCE_PERCENTAGE * elapsed as f64) as i64;
+	assert_near(elapsed, sw.elapsed_ms().to_i64().unwrap(), tolerance_value);
 }
