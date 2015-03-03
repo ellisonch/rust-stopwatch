b131e3c8546961284592d46a929ba39c6477efe5
Justin Latimer <justinlatimer@gmail.com>
2015-03-03 07:26:47 +0000

    Some whitespace.

--- tests/lib.rs
+++ tests/lib.rs
@@ -47,7 +47,7 @@ fn stop() {
 fn resume_once() {
 	let mut sw = Stopwatch::start_new();
 	timer::sleep(Duration::milliseconds(SLEEP_MS));
-	sw.stop();	
+	sw.stop();
 	assert_sw_near(sw, SLEEP_MS);
 	sw.start();
 	timer::sleep(Duration::milliseconds(SLEEP_MS));
@@ -58,7 +58,7 @@ fn resume_once() {
 fn resume_twice() {
 	let mut sw = Stopwatch::start_new();
 	timer::sleep(Duration::milliseconds(SLEEP_MS));
-	sw.stop();	
+	sw.stop();
 	assert_sw_near(sw, SLEEP_MS);
 	sw.start();
 	timer::sleep(Duration::milliseconds(SLEEP_MS));
